fb57c32588e9709fb28dd5d12f622682c3ac074c
Maria Tkacheva <tkachevamasha35@yandex.ru>
2023-05-02 13:22:18 +0300

    JS-smooth-scroll

--- js.js
+++ js.js
@@ -29,7 +29,6 @@
             navMenu.classList.remove('right_menu')
         })
     });
-    console.log(navLink)
     burgerItem.addEventListener('click',()=>{
         navMenu.classList.add('right_menu')
     })
@@ -37,4 +36,24 @@
         navMenu.classList.remove('right_menu')
     })
 
+}());
+
+
+(function(){
+    document.querySelectorAll('a[href^="#"').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();//предопределяет поведение браузера
+        let href = this.getAttribute('href').substring(1);
+        const scrollTarget = document.getElementById(href);
+
+        const topOffset = document.querySelector('nav').offsetHeight;
+        const elementPosition = scrollTarget.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - topOffset;
+        
+        window.scrollBy({
+            top: offsetPosition,
+            behavior: 'smooth'
+        });
+    });
+})
 }())
